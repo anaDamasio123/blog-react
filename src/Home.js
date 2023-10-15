4ff1b8f35a9f2c912e6f54596b86526a75632908
Ana Damasio <ana_damasio@proton.me>
2023-10-15 19:46:24 +0100

    json server

--- src/Home.js
+++ src/Home.js
@@ -8,7 +8,6 @@ const Home  = () => {
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
       ]);
 
-      const [name, setName] = useState('mario');
     
     const handleDelete = id => {
         const newBlogs = blogs.filter(blog => blog.id !== id);  // Doesn't change the original array 'blogs'
@@ -16,14 +15,12 @@ const Home  = () => {
     }
 
     useEffect(() => {  // Runs everytime there is a re-render
-        console.log(name);
-    }, [name]);
+        console.log('use effect ran');
+    }, []);
 
     return ( 
         <div className="home">
             <BlogList blogs={blogs} title="All my blogs!" handleDelete={handleDelete} />
-            <button onClick={() => setName('luigi')}>change name</button>
-            <p>{ name }</p>
         </div>
      );
 }
