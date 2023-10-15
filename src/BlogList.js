386548a23e18f7f3caa958527dd77fe449438c1b
Ana Damasio <ana_damasio@proton.me>
2023-10-15 15:31:19 +0100

    reusable BlogList Component

--- src/BlogList.js
+++ src/BlogList.js
@@ -6,7 +6,7 @@ const BlogList = ({ blogs, title }) => {
             { blogs.map((blog) => (
                 <div className="blog-preview" key={blog.id}>
                     <h2>{ blog.title }</h2>
-                    <p>{ blog.author }</p>
+                    <p>Written by { blog.author }</p>
                 </div>
             ))}
         </div>
