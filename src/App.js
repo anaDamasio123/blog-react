a3ce9082f16726920420e3062331df56e355c365
Ana Damasio <ana_damasio@proton.me>
2023-10-21 21:15:33 +0100

    route parameters

--- src/App.js
+++ src/App.js
@@ -2,13 +2,9 @@ import Navbar from './Navbar';
 import Home from './Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Create from './Create';
+import BlogDetails from './BlogDetails';
 
 function App() {
-  const title = 'Welcome to the new blog';
-  const likes = 50;
-  //const person = { name: 'yoshi', age: 30 };
-  const link = "http://www.google.com";
-
   return (
     <Router>
       <div className="App">
@@ -20,6 +16,9 @@ function App() {
             </Route>
             <Route path="/create">
               <Create />
+            </Route>
+            <Route path="/blogs/:id">
+              <BlogDetails />
             </Route>
           </Switch>
         </div>
